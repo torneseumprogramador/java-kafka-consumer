5a8a0f9b1034edc6e12c265b45018e0e6cc11960
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-05-07 13:30:16 -0300

    novos

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-export KAFKA_HOST="192.168.0.19:9092"
+export KAFKA_HOST="localhost:9092"
 export KAFKA_TOPIC="TOPICO_1"
 export KAFKA_GROUP_ID_READER="consome_1"
 
-export CLASSPATH=target/kafka_consumer-1.0-SNAPSHOT.jar
 export className=App
 echo "## Running $className..."
-shift
 mvn exec:java -Dexec.mainClass="br.com.kafka_consumer.$className"
